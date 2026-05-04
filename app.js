f10522d6f9e96a83259bfae0c1d9865fef123865
mapsmania <37103836+mapsmania@users.noreply.github.com>
2026-05-04 09:46:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const map = new maplibregl.Map({
     sources: {},
     layers: []
   },
-  center: [-96.5, 38.5], 
-  zoom: 4
+  center: [-98.5, 39.8],
+  zoom: 3
 });
 
 // -----------------------------
